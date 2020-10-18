4ac783e9dbe3122985267fd7f6fbca9c25a727bb
[mr.brain1201] <[mr.brain1201@gmail.com]>
2020-10-18 17:07:17 +0300

    ex_1

---
+++ lesson_4/ex_1.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+while True:
+    try:
+        script_name, first_param, second_param, third_param = argv
+        for i in argv[1:-1]:
+            i.isdigit()
+            if i.isdigit() is False:
+                print('Введите числовые значения!')
+                break
+        print(f'Сумма к выплате: {float(first_param) * float(second_param) + float(third_param)}')
+        break
+    except ValueError as err:
+        print(err)
+        break
